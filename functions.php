73ee1dd5faebff0b8df9c00a392578c34b620a67
Nenad Nikolic <nendn.dizajn@gmail.com>
2023-04-12 20:37:21 +0200

    Custom Block init 2

--- functions.php
+++ functions.php
@@ -41,3 +41,8 @@ function include_inc_files() {
 }
 
 include_inc_files();
+
+add_action( 'init', 'register_acf_blocks' );
+function register_acf_blocks() {
+    register_block_type( __DIR__ . '/blocks/belov-test-block' );
+}
